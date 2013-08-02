cc5b96e54dbf9df6ceb22aab72522c3acc48c77a
mahinshaw <mahinshaw@gmail.com>
2013-08-02 10:57:21 -0400

    updates to code base

---
+++ js/content.js
@@ -0,0 +1,20 @@
+//"use strict";
+
+$("#slide4button").click(CSVmotionHeat("div.modalheat4"));
+$("#slide5button").click(CSVmotionHeat("div.modalheat5"));
+
+var modalSurveyButton9 = $("#modalSurveyButton9"),
+    ms9content = $("#ms9content").detach(),
+    modalSurveyButton6 = $("#modalSurveyButton6");
+            
+modalSurveyButton9.click(function(){
+    $("#ms9survey").detach();
+    ms9content.appendTo("#ms9body")
+    modalSurveyButton9.detach();
+});
+            
+modalSurveyButton6.click(function(){
+    $("#ms6survey").detach();
+    CSVmotionHeat("div.heat6");
+    modalSurveyButton.detach();
+});
